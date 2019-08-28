7bf70a89b68711fa7d63f9451b476a1ce8fffd8a
YS <yorik_2009@mail.ru>
2019-08-28 19:39:45 +0300

    Release commit v3.1

--- src/main/java/by/ps/rstelegrambot/bot/GuideBot.java
+++ src/main/java/by/ps/rstelegrambot/bot/GuideBot.java
@@ -1,89 +1,89 @@
-package by.ps.rstelegrambot.bot;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.TelegramBotsApi;
-import org.telegram.telegrambots.api.methods.send.SendMessage;
-import org.telegram.telegrambots.api.objects.Message;
-import org.telegram.telegrambots.api.objects.Update;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.exceptions.TelegramApiException;
-import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
-import javax.annotation.PostConstruct;
-
-@Component
-@PropertySource("classpath:bot.properties")
-public class GuideBot extends TelegramLongPollingBot {
-
-    @Value("${bot.username}")
-    private String botUsername;
-
-    @Value("${bot.token}")
-    private String botToken;
-
-    @Value("${api.url.prefix}")
-    private String apiUrlPrefix;
-
-    @Autowired
-    ServerResponse serverResponse;
-
-//    static {
-//        ApiContextInitializer.init();
+//package by.ps.rstelegrambot.bot;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.PropertySource;
+//import org.springframework.stereotype.Component;
+//import org.telegram.telegrambots.ApiContextInitializer;
+//import org.telegram.telegrambots.TelegramBotsApi;
+//import org.telegram.telegrambots.api.methods.send.SendMessage;
+//import org.telegram.telegrambots.api.objects.Message;
+//import org.telegram.telegrambots.api.objects.Update;
+//import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+//import org.telegram.telegrambots.exceptions.TelegramApiException;
+//import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+//import javax.annotation.PostConstruct;
+//
+//@Component
+//@PropertySource("classpath:bot.properties")
+//public class GuideBot extends TelegramLongPollingBot {
+//
+//    @Value("${bot.username}")
+//    private String botUsername;
+//
+//    @Value("${bot.token}")
+//    private String botToken;
+//
+//    @Value("${api.url.prefix}")
+//    private String apiUrlPrefix;
+//
+//    @Autowired
+//    ServerResponse serverResponse;
+//
+////    static {
+////        ApiContextInitializer.init();
+////    }
+//
+//    @PostConstruct
+//    public void initBot() {
+//        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+//        try {
+//            telegramBotsApi.registerBot(this);
+//        } catch (TelegramApiRequestException e) {
+//            e.printStackTrace();
+//        }
 //    }
-
-    @PostConstruct
-    public void initBot() {
-        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
-        try {
-            telegramBotsApi.registerBot(this);
-        } catch (TelegramApiRequestException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void onUpdateReceived(Update update) {
-
-        Message message = update.getMessage();
-
-        if (message != null && message.hasText()) {
-            String chatId = update.getMessage().getChatId().toString();
-            String msg = update.getMessage().getText();
-
-            if (msg.equals("/start")) {
-                sendMsg(chatId, "Hello! Please enter the city name in chat " +
-                        "For example: \"Moscow\" or \"London\"");
-            } else {
-                String text = this.serverResponse.getResponse(apiUrlPrefix + msg);
-                sendMsg(chatId, text);
-            }
-        }
-    }
-
-    public synchronized void sendMsg(String chatId, String text){
-
-        SendMessage message = new SendMessage();
-        message.enableMarkdown(true);
-        message.setChatId(chatId);
-        message.setText(text);
-
-        try {
-            execute(message);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public String getBotUsername() {
-        return botUsername;
-    }
-
-    @Override
-    public String getBotToken() {
-        return botToken;
-    }
-}
+//
+//    @Override
+//    public void onUpdateReceived(Update update) {
+//
+//        Message message = update.getMessage();
+//
+//        if (message != null && message.hasText()) {
+//            String chatId = update.getMessage().getChatId().toString();
+//            String msg = update.getMessage().getText();
+//
+//            if (msg.equals("/start")) {
+//                sendMsg(chatId, "Hello! Please enter the city name in chat " +
+//                        "For example: \"Moscow\" or \"London\"");
+//            } else {
+//                String text = this.serverResponse.getResponse(apiUrlPrefix + msg);
+//                sendMsg(chatId, text);
+//            }
+//        }
+//    }
+//
+//    public synchronized void sendMsg(String chatId, String text){
+//
+//        SendMessage message = new SendMessage();
+//        message.enableMarkdown(true);
+//        message.setChatId(chatId);
+//        message.setText(text);
+//
+//        try {
+//            execute(message);
+//        } catch (TelegramApiException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    @Override
+//    public String getBotUsername() {
+//        return botUsername;
+//    }
+//
+//    @Override
+//    public String getBotToken() {
+//        return botToken;
+//    }
+//}
---
+++ src/main/java/by/ps/rstelegrambot/bot/WebHookBot.java
@@ -0,0 +1,77 @@
+package by.ps.rstelegrambot.bot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.BotApiMethod;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramWebhookBot;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@PropertySource("classpath:bot.properties")
+public class WebHookBot extends TelegramWebhookBot {
+
+    @Value("${bot.username}")
+    private String botUsername;
+
+    @Value("${bot.token}")
+    private String botToken;
+
+    @Value("${api.url.prefix}")
+    private String apiUrlPrefix;
+
+    @Autowired
+    ServerResponse serverResponse;
+
+    @PostConstruct
+    public void initBot() {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(this);
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public BotApiMethod onWebhookUpdateReceived(Update update) {
+
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage sendMessage = new SendMessage();
+            sendMessage.setChatId(update.getMessage().getChatId().toString());
+
+            String msg = update.getMessage().getText();
+
+            if (msg.equals("/start")) {
+                sendMessage.setText("Hello! Please enter the city name in chat " +
+                        "For example: \"Moscow\" or \"London\"");
+            } else {
+                String text = this.serverResponse.getResponse(apiUrlPrefix + msg);
+                sendMessage.setText(text);
+            }
+            return sendMessage;
+        }
+        return null;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public String getBotPath() {
+        return "https://guidetelegrambot.herokuapp.com";
+    }
+}
