5f377187a830e5fb857736b91f80345a1aa2c4ef
YS <yorik_2009@mail.ru>
2019-08-29 12:39:49 +0300

    Release commit v3.6

---
+++ src/main/java/by/ps/rstelegrambot/controller/WebHookController.java
@@ -0,0 +1,16 @@
+//package by.ps.rstelegrambot.controller;
+//
+//import org.springframework.http.MediaType;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/")
+//public class WebHookController {
+//
+//    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+//    public String telegramUpdates() {
+//
+//    }
+//}
